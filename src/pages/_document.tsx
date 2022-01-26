02a0a9ea9fbe99abc9c402d8a605df73ae3c9e89
Samuel Corsi-House <chouse.samuel@gmail.com>
2022-01-26 16:25:43 -0500

    fix: some lighthouse improvements

---
+++ src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import NextDocument, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext
+} from 'next/document';
+
+class Document extends NextDocument {
+  public static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await NextDocument.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  public render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default Document;
